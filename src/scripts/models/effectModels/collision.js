b3bef629c361874f9eb6bfb1cc5abb1588081f4d
shapirab <shapirab@jamd.ac.il>
2026-02-23 14:25:48 +0200

    23/2/26 added collision detector function

---
+++ src/scripts/models/effectModels/collision.js
@@ -0,0 +1,12 @@
+export default class CollisionDetector{
+    constructor(game){
+        this.game = game;
+    }
+
+    rectCollisionDetector(rect_1, rect_2){
+        return rect_1.position.x + rect_1.width > rect_2.position.x ||
+                rect_1.position.x < rect_2.position.x + rect_2.width ||
+                rect_1.position.y < rect_2.position.y + rect_2.height ||
+                rect_1.position.y + rect_1.height > rect_2.position.y
+    } 
+}
