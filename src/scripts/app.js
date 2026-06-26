c4ff0ae0de9cc2476f6bdf693678e953e0f355b1
shapirab <shapirab@jamd.ac.il>
2026-06-26 15:00:42 +0300

    26/6/26 developed start game functionality

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,11 +2,31 @@ import Game from "./game.js";
 
 window.addEventListener('load', () => {
     const canvas = document.getElementById('main-canvas');
+    const restartButton = document.querySelector('.btn');
+
     canvas.width = 1200;
     canvas.height = 700;
     let ctx = canvas.getContext('2d');
 
-    const game = new Game(canvas.width, canvas.height);   
+    const game = new Game(canvas.width, canvas.height);
+
+    restartButton.addEventListener('click', () => {
+        restartButton.blur();
+        hideRestartBtn();
+        game.resetGame();
+        animate(0);
+    });
+
+    function displayRestartBtn(){
+        console.log('app::displayRestartBtn')
+        restartButton.classList.remove('hidden');
+        restartButton.classList.add('show');;
+        console.log(restartButton)
+    }
+    function hideRestartBtn(){
+        restartButton.classList.remove('show');
+        restartButton.classList.add('hidden');
+    }
 
     let lastTime = 0;
     function animate(timeStemp){
@@ -15,6 +35,10 @@ window.addEventListener('load', () => {
         lastTime = timeStemp;
         game.update(deltatime);
         game.draw(ctx);
+        if(game.gameOver){
+            console.log('gameOver called in app')
+            displayRestartBtn();
+        }
         if(!game.gameOver){
             requestAnimationFrame(animate);
         }
